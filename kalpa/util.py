ad1f2b7e45237e07ea21575c5f370c1f60325762
Elmer de Looff <elmer.delooff@gmail.com>
2016-07-24 16:38:55 +0100

    Added utlity functions to retrieve parent resources.

--- kalpa/util.py
+++ kalpa/util.py
@@ -32,3 +32,23 @@ def memoize(wrapped):
             cache[item] = wrapped(inst, item)
         return cache[item]
     return functools.update_wrapper(decorator, wrapped)
+
+
+def parent_by_name(resource, parent_name):
+    """Returns the first parent whose name matches the query."""
+    return _find_parent(resource, lambda res: res.__name__ == parent_name)
+
+
+def parent_by_class(resource, parent_cls):
+    """Returns the first parent to match the given class (type or name)."""
+    if isinstance(parent_cls, type):
+        return _find_parent(resource, lambda res: type(res) is parent_cls)
+    return _find_parent(resource, lambda res: type(res).__name__ == parent_cls)
+
+
+def _find_parent(child_resource, predicate):
+    """Returns resource's first parent that fulfills the predicate function."""
+    for resource in lineage(child_resource):
+        if predicate(resource):
+            return resource
+    raise LookupError('No matching parent in resource lineage.')
