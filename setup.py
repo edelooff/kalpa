afe4796621360bce344e8ce81a052d723841b3f1
Elmer de Looff <elmer.delooff@gmail.com>
2017-09-12 00:05:32 +0200

    Fixed trove classifiers and re-ordered setuptools keywords.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import os
 from setuptools import setup, find_packages
 
@@ -18,18 +16,17 @@ setup(
     author_email='elmer.delooff@gmail.com',
     description='Resource baseclasses for traversal in Pyramid ',
     long_description=contents('README.rst'),
-    url='http://variable-scope.com',
     keywords='pyramid traversal resource helper',
+    license='BSD',
+    url='https://github.com/edelooff/kalpa',
+    install_requires=['six'],
+    extras_require={
+        'test': ['pyramid', 'pytest-runner', 'pytest']},
+    zip_safe=False,
     classifiers=[
-        'Development Status :: 2 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Framework :: Pyramid',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP'],
-    zip_safe=False,
-    install_requires=['six'],
-    extras_require={
-        'test': ['pyramid', 'pytest-runner', 'pytest']
-    },
-)
+        'Topic :: Internet :: WWW/HTTP'])
